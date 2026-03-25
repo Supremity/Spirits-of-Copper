e6eec88d36b1a6fc74ab5cb6a4a49c14fdfcc8c5
ZaneAhmed <zane.mahmoud212@gmail.com>
2026-03-25 23:11:28 +0100

    Fixed linear stuff

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -2,12 +2,12 @@ extends Node3D
 class_name Main
 @onready var culture_sprite: Sprite2D = $MapViewport/MapContainer/CultureSprite
 @export var clock: GameClock
-@onready var camera_controller: Node = $Camera3D/CameraController
+@onready var camera_3d: Camera3D = $Camera3D
 
 
 func _enter_tree() -> void:
 	GameState.main = self
-	GameState.camera = camera_controller
+	GameState.camera = camera_3d
 
 
 func _ready() -> void:
